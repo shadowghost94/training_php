3e67ca20c19330da583b6c0c743f36c5108b555d
salifououzerou25@gmail.com <salifououzerou25@gmail.com>
2023-09-26 15:23:16 +0100

    php_type-settype()-gettype()-is_int-is_double-is_long-is_array-is_object-is_string

---
+++ chapitre11.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>les types</title>
+</head>
+<body>
+    <?php
+        echo "les types classique rencontrés en php sont:"."</br>";
+        /*
+            *integer
+            *double
+            *string
+            *array
+            *object
+            *class
+            *unknown type
+
+            les fontions settype() et gettype() permettent respectivement
+             de récupérer et de modifier le type d'une varibale
+
+            Il existe d'autres fonctions similaires qu'on peut utiliser 
+            pour la vérification de type:
+                *is_int()
+                *is_double()
+                *is_long()
+                *is_array()
+                *is_string()
+                *is_object()
+
+            
+        */ 
+    ?>
+</body>
+</html>
